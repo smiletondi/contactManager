0ef3423b16efaefb45c76799b46414798d353748
smiletondi <smiletondi@gmail.com>
2019-06-29 12:09:52 +0100

    Updates

---
+++ precache-manifest.a0434e9a5f82455a7bff4e7169f9233e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa09cfd88d3f07f578dc57b9b83f386",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3e67c4629e049e9488a5",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2e2cf64cbab1d99c0b25",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3e67c4629e049e9488a5",
+    "url": "/contactmanager/static/js/2.acc9fd93.chunk.js"
+  },
+  {
+    "revision": "2e2cf64cbab1d99c0b25",
+    "url": "/contactmanager/static/js/main.e5a34f5b.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
